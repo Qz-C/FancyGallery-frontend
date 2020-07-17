eaaaf4f2b723b8549dcb0ea685c25a01af64d5a8
Ezequiel Fonseca <31161704+Qz-C@users.noreply.github.com>
2020-07-17 07:06:28 -0300

    src/App.js
    modifications done at App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './global.css';
+import Login from './pages/Login'
 
 function App() {
   return (
     <div className="App">
-      Start Page
+      < Login/>
     </div>
   );
 }
