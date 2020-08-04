d8b52bb062020db93e658d3b3a1579ce7a23e29f
Ezequiel Fonseca <31161704+Qz-C@users.noreply.github.com>
2020-08-04 19:37:12 -0300

    New route implemented

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Profile from './pages/Profile'
 
 const Routes = () => {
     return(
@@ -10,6 +11,7 @@ const Routes = () => {
             <Switch>
                 <Route path="/" exact component={Login} />
                 <Route path="/register" component={Signup} />
+                <Route path="/profile" component={Profile}/> 
             </Switch>
         </BrowserRouter>
     );
