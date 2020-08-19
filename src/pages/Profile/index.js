a8d59ed7a51acfc5e24297225d1ac860d2c2c322
Ezequiel Fonseca <31161704+Qz-C@users.noreply.github.com>
2020-08-19 01:31:18 -0300

    finishing

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,7 @@
 import React,{ useState, useEffect } from "react";
 
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import "./styles.css";
 
 import '../../global.css';
@@ -11,19 +13,23 @@ import { useHistory, Link } from "react-router-dom";
 import cookie from "../../services/cookies"
 
 import HeaderProfile from "../../components/HeaderProfile";
+
+import Loading from "../../components/Loading"
  
 const Profile = () => {
 
     const SERVER_BASE_URL = 'http://localhost:3333/'
-
+    const itemsPerPage = 9;
     const history = useHistory();
     const [token, setToken] = useState(cookie.getCookie("token"));
     const [user, setUser] = useState();
     const [pictures, setPictures] = useState([]);
     const [page, setPage] = useState(1);
+    const [keepShowingScroll , setKeepShowingScroll] = useState(true)
 
     //On first load and logout
     useEffect( () => {
+        console.log("User running...")
         if( token === "")
             history.push('/');
 
@@ -36,43 +42,67 @@ const Profile = () => {
         }).catch( () => {
             history.push('/');
         })
-        console.log("user rodou")
-    }, [token]);
+        console.log("User finished !!!")
+    }, [token, history]);
 
     //Load the pictures
     useEffect ( () => {
-        api.get(`/img/list?page=${page}`, {
-            headers: {
-                Authorization : `Bearer ${token}`}
-            }).then ( response => {
-                setPictures(...pictures, response.data.imgList);
-                console.log(response.data);
-            }).catch (error => {
-                console.log(error)
-            })
-            console.log("img rodou");
-        }, [page]);
+            
+        },[]);
+
+        //
+        const fetchData = () => {
+                console.log()
+                api.get(`/img/list?page=${page}&items=${itemsPerPage}`, {
+                headers: {
+                    Authorization : `Bearer ${token}`}
+                }).then ( response => {
+                    if(response.data !== [])
+                    {
+                        setPage(page+1);
+                        setPictures([...pictures, ...response.data.imgList]);
+                        console.log(response.data);
+                    }else{
+                        setKeepShowingScroll(false);
+                        console.log("Ñ mexi no pictures");
+                    }
+                        
+                        
+                }).catch (error => {
+                    console.log(error)
+                })
+        }
 
     return(
 
         <div id="container-profile">
             { user && <HeaderProfile name={user.name}/>}
-            <main className="gallery">
-                {pictures.map(picture => {
+            <InfiniteScroll 
+                dataLength={itemsPerPage}
+                next={fetchData}
+                hasMore={ keepShowingScroll }
+                loader={<Loading/>}
+                endMessage={<h1>END !</h1>}
+            >
+                
+                {console.log(keepShowingScroll)}
+                <main className="gallery">
+                    {pictures.map(picture => {
 
-                        const img = {
-                            background:`url(${SERVER_BASE_URL}${user.email}/${picture.name}) no-repeat center center`
-                        } 
+                            const img = {
+                                background:`url(${SERVER_BASE_URL}${user.email}/${picture.name}) no-repeat center center`
+                            } 
 
-                        return(
-                            <Link style={img} className="link" to="#" key={picture.id}>
-                               <div className="img">
+                            return(
+                                <Link style={img} className="link" to="#" key={picture.id}>
+                                <div className="img">
 
-                               </div>
-                            </Link>
-                        )
-                })}
-            </main>
+                                </div>
+                                </Link>
+                            )
+                    })}
+                </main>
+            </InfiniteScroll>
         </div>
     )
 }
